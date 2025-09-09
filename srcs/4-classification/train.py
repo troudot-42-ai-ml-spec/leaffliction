502afe532250a15c985ad936af93a1df91ea5857
Supa96z <112761949+Supa96z@users.noreply.github.com>
2025-09-09 18:17:08 +0200

    made check_dir to make sure input directory is correctly formated.

--- srcs/4-classification/train.py
+++ srcs/4-classification/train.py
@@ -1,4 +1,5 @@
 # from utils import split_data
+from utils import check_dir
 # from pathlib import Path
 import argparse
 # import tensorflow as tf
@@ -10,17 +11,18 @@ import argparse
 # Hyperparameters
 IMG_HEIGHT = 128
 IMG_WIDTH = 128
-IMG_DEPTH = 3
-NUM_CLASSES = 8
+CHANNELS = 3
+CLASSES = 8
 EPOCHS = 20
 
 
 # def build_model(input_path: str):
+
 #     model = keras.Sequential(
 #         [
-#             layers.Input(shape=(IMG_HEIGHT, IMG_WIDTH, IMG_DEPTH)),
-#             layers.Conv2D(),
-#             layers.MaxPool2D()
+#             layers.Input(shape=(IMG_HEIGHT, IMG_WIDTH, CHANNELS)),
+#             layers.SeparableConv2D(24, 3, activation='relu'),
+#             layers.MaxPooling2D()
 #         ]
 #     )
 
@@ -28,8 +30,12 @@ EPOCHS = 20
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("input_path", type=str)
-    # args = parser.parse_args()
-    # build_model(args.input_path)
+    args = parser.parse_args()
+    try:
+        check_dir(args.input_path)
+        # build_model(args.input_path)
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
--- srcs/4-classification/utils/__init__.py
+++ srcs/4-classification/utils/__init__.py
@@ -1,1 +1,2 @@
 from .split import split_data
+from .check_dir import check_dir

60c6901fa867d79e7410be575724bcc530e34222
lucas-ht <46166712+lucas-ht@users.noreply.github.com>
2025-09-15 01:06:29 +0200

    chore: remove dead training code

--- srcs/__init__.py
+++ srcs/__init__.py
@@ -1,11 +1,7 @@
-from srcs.utils.split_data import split_data
-from srcs.utils.load_datasets import load_datasets
 from srcs.utils.build_model import build_model
 from srcs.utils.train_model import train_model
 
 __all__ = [
-    "split_data",
-    "load_datasets",
     "build_model",
     "train_model",
 ]

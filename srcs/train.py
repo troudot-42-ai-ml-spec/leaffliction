428f9c3ac2ad491e63b42c01e275d68a0d08569c
lucas-ht <46166712+lucas-ht@users.noreply.github.com>
2025-09-15 00:59:36 +0200

    feat: data pipeline integration

--- srcs/train.py
+++ srcs/train.py
@@ -1,44 +1,86 @@
-import os
 import argparse
+import tensorflow as tf
+from utils.augmentation import augment_dataset
+from utils.hyperparams import IMG_HEIGHT, IMG_WIDTH, BATCH_SIZE
+from utils.build_model import build_model
+from utils.train_model import train_model
 
 
-def main(args):
-    from srcs.utils import (
-        check_dir,
-        split_data,
-        load_datasets,
-        build_model,
-        train_model,
+def main() -> None:
+    """
+    Main function to load dataset, augment it, build and train the model.
+    """
+    # https://github.com/tensorflow/tensorflow/issues/68593
+    import os
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "dataset_path", help="The path to the dataset directory."
     )
+    args = parser.parse_args()
 
     try:
-        # --- Check directory validity ---
-        check_dir(args.input_path)
-        # --- Initial train/test split ---
-        train_set, test_set = split_data(args.input_path)
-        # --- Rafined split into train/val/test ---
-        t_set, v_set, tt_set = load_datasets(train_set, test_set)
-        # --- Build model ---
-        model = build_model()
-        # --- Compile and Train ---
-        history = train_model(model, t_set, v_set, tt_set)
-        print(history)  # linter for commit
-        # --- Plot training metrics ---
-        # plot_metrics(history)
+        print("⏳ Loading original dataset...")
+        dataset = tf.keras.utils.image_dataset_from_directory(
+            directory=args.dataset_path,
+            labels="inferred",
+            label_mode="int",
+            image_size=(IMG_HEIGHT, IMG_WIDTH),
+            batch_size=None,
+        )
+        print("✅ Original dataset loaded.")
+        class_names = dataset.class_names
 
+        # ! The whole dataset should NOT be augmented, only the training set.
+        # ! If only the training set is augmented, the accuracy plummets
+        # ! from ~95% to ~10%. It seems that the model overfits the augmented data
+        # ! and fails to generalize to the original data.
+        # * Comment the line below to disable dataset augmentation.
+        dataset = augment_dataset(dataset)
+
+        # Split into train (70%), val (20%), test (10%)
+        print("⏳ Splitting dataset...")
+        train_dataset, remaining_dataset = tf.keras.utils.split_dataset(
+            dataset, left_size=0.7, shuffle=True, seed=42
+        )
+        validation_dataset, test_dataset = tf.keras.utils.split_dataset(
+            remaining_dataset, left_size=0.67, shuffle=True, seed=42
+        )
+
+        train_dataset.class_names = class_names
+        validation_dataset.class_names = class_names
+        test_dataset.class_names = class_names
+
+        # * Uncomment the line below to augment the training set.
+        # train_dataset = augment_dataset(train_dataset)
+        # TODO: Force train_dataset evaluation
+
+        train_dataset = train_dataset.cache()               \
+            .batch(BATCH_SIZE)                              \
+            .prefetch(tf.data.AUTOTUNE)
+        validation_dataset = validation_dataset.cache()     \
+            .batch(BATCH_SIZE)                              \
+            .prefetch(tf.data.AUTOTUNE)
+        test_dataset = test_dataset.cache()                 \
+            .batch(BATCH_SIZE)                              \
+            .prefetch(tf.data.AUTOTUNE)
+
+        model = build_model()
+        train_model(
+            model,
+            train_dataset,
+            validation_dataset,
+            test_dataset,
+        )
+
+        # TODO: Save the model in a zip with the dataset
         os.makedirs("models", exist_ok=True)
         model.save("models/leaffliction.keras", overwrite=True)
-
     except Exception as e:
-        print(e)
+        print(f"An error occurred: {e}")
+        return
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "input_path",
-        type=str,
-        help="Path to the fully augmented and transformed dataset.",
-    )
-    args = parser.parse_args()
-    main(args)
+    main()

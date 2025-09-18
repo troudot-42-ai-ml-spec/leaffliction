715e29c965636695de92b2e3c8fced9333193ea7
lucas-ht <46166712+lucas-ht@users.noreply.github.com>
2025-09-18 02:20:24 +0200

    chore: remove dead class labels

--- srcs/utils/hyperparams.py
+++ srcs/utils/hyperparams.py
@@ -3,14 +3,3 @@ IMG_WIDTH = 128
 BATCH_SIZE = 32
 CLASSES = 8
 EPOCHS = 20
-
-CLASS_LABELS = [
-    "Apple_Black_rot",
-    "Apple_healthy",
-    "Apple_rust",
-    "Apple_scab",
-    "Grape_Black_rot",
-    "Grape_Esca",
-    "Grape_healthy",
-    "Grape_spot",
-]

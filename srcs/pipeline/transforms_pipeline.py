5907bf20987dc7b62192fac9a3bff1a8b5bb4164
0x21x <114038240+0x21x@users.noreply.github.com>
2025-09-21 17:04:13 +0200

    add multithreading

--- srcs/pipeline/transforms_pipeline.py
+++ srcs/pipeline/transforms_pipeline.py
@@ -6,7 +6,9 @@ from transforms.registry import build, available_ops
 from typing import List, Optional, Dict, Any
 from plantcv import plantcv as pcv
 from utils.plotting.grid import show_grid
-import time
+import multiprocessing as mp
+from tqdm import tqdm
+
 
 _OPS: List[Transformation] = []
 
@@ -16,6 +18,15 @@ def build_ops(ops_list: List[str]) -> None:
         if op not in available_ops():
             raise Exception(f"{op} is not a valid operation!")
         _OPS.append(build(op))
+
+
+def _build_ops(ops_list: List[str]) -> List[Transformation]:
+    ops: List[Transformation] = []
+    for op in ops_list:
+        if op not in available_ops():
+            raise Exception(f"{op} is not a valid operation!")
+        ops.append(build(op))
+    return ops
 
 
 def get_ops() -> List[Transformation]:
@@ -55,31 +66,71 @@ def extract_variants(
     }
 
 
+def process_single_image(
+    img: np.ndarray,
+    path: Path,
+    args: Namespace,
+    ops: List[Transformation],
+    dst: Optional[Path] = None,
+) -> None:
+    ctx: Dict[str, Any] = {}
+    for op in ops:
+        img = op.apply(img, ctx)
+
+    variants = extract_variants(img, ctx)
+
+    if args.show:
+        show_grid(
+            list(variants.values()), list(variants.keys()), max_cols=len(variants)
+        )
+    if dst:
+        for name, variant in variants.items():
+            # dst/<classe>/<variant>/...
+            save_path = dst / path.parent.name / name
+            save_path.mkdir(parents=True, exist_ok=True)
+            save_path = save_path / f"{path.stem}{path.suffix}"
+            pcv.print_image(variant, str(save_path.absolute()))
+            # print(f"Saved {path.name} in {time.time() - start_time:.2f}s")
+
+
+def process(task_queue: mp.Queue, results_queue: mp.Queue, args, dst):
+    ops = _build_ops(args.ops)
+    while True:
+        path = task_queue.get()
+        if path is None:
+            break
+        # start_time = time.time()
+        pcv.outputs.clear()
+        img, _, _ = pcv.readimage(filename=str(path.absolute()))
+        process_single_image(img, path, args, ops, dst)
+        # print(f"Processed {path.name} in {time.time() - start_time:.2f}s")
+        results_queue.put(path)
+
+
 def pipeline(
     path_list: List[Path], args: Namespace, dst: Optional[Path] = None
 ) -> None:
     if not _OPS:
-        raise Exception("You have build first the ops!")
+        build_ops(args.ops)
+
+    task_queue = mp.Queue()
+    results_queue = mp.Queue()
 
     for path in path_list:
-        start_time = time.time()
-        img, _, _ = pcv.readimage(filename=str(path.absolute()))
-        ctx: Dict[str, Any] = {}
-        for op in _OPS:
-            img = op.apply(img, ctx)
+        task_queue.put(path)
 
-        variants = extract_variants(img, ctx)
+    num_processes = mp.cpu_count() - 2 if mp.cpu_count() > 2 else 1
+    for _ in range(num_processes):
+        task_queue.put(None)
 
-        if args.show:
-            show_grid(
-                list(variants.values()), list(variants.keys()), max_cols=len(variants)
-            )
-        if dst:
-            for name, variant in variants.items():
-                # dst/<classe>/<variant>/...
-                save_path = dst / path.parent.name / name
-                save_path.mkdir(parents=True, exist_ok=True)
-                save_path = save_path / f"{path.stem}{path.suffix}"
-                pcv.print_image(variant, str(save_path.absolute()))
-            print(f"Saved {path.name} in {time.time() - start_time:.2f}s")
-        pcv.outputs.clear()
+    processes = []
+    for _ in range(num_processes):
+        p = mp.Process(target=process, args=(task_queue, results_queue, args, dst))
+        p.start()
+        processes.append(p)
+
+    for _ in tqdm(range(len(path_list)), desc="Processing images"):
+        results_queue.get()
+
+    for p in processes:
+        p.join()
